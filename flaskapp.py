cf43e3aadede88651f3d559e95bc43213558ac4c
Matthew Hill <mhill@stsci.edu>
2017-05-19 12:12:48 -0400

    more functionality

--- flaskapp.py
+++ flaskapp.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello from Flask!'
 
+@app.route('/countme/<input_str>')
+def count_me(input_str):
+    return input_str
+    
 if __name__ == '__main__':
   app.run()

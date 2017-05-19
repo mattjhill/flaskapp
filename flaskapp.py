e0bc6c32fef915257fe4cec79aad620f7e42d0ce
Matthew Hill <mhill@stsci.edu>
2017-05-19 12:27:35 -0400

    more functionalilty

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,24 @@
 from flask import Flask
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
 app = Flask(__name__)
+
+colors = {
+    'Black': '#000000',
+    'Red':   '#FF0000',
+    'Green': '#00FF00',
+    'Blue':  '#0000FF',
+}
+
+def getitem(obj, item, default):
+    if item not in obj:
+        return default
+    else:
+        return obj[item]
 
 @app.route('/')
 def hello_world():
@@ -8,6 +27,43 @@ def hello_world():
 @app.route('/countme/<input_str>')
 def count_me(input_str):
     return input_str
-    
+
+@app.route('/exotransmit/')
+def exotransmit():
+
+@app.route("/bokeh/")
+def polynomial():
+    """ Very simple embedding of a polynomial chart
+    """
+
+    # Grab the inputs arguments from the URL
+    args = flask.request.args
+
+    # Get all the form arguments in the url with defaults
+    color = getitem(args, 'color', 'Black')
+    _from = int(getitem(args, '_from', 0))
+    to = int(getitem(args, 'to', 10))
+
+    # Create a polynomial line graph with those arguments
+    x = list(range(_from, to + 1))
+    fig = figure(title="Polynomial")
+    fig.line(x, [i ** 2 for i in x], color=colors[color], line_width=2)
+
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    script, div = components(fig)
+    html = flask.render_template(
+        'embed.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        color=color,
+        _from=_from,
+        to=to
+    )
+    return encode_utf8(html)
+
 if __name__ == '__main__':
   app.run()
